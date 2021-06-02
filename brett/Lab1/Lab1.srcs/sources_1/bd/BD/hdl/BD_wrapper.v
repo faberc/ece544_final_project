1a2c9d930581459d5ec9c1844436b169939f9bf1
bthorn2 <bthorn2@pdx.edu>
2021-06-01 18:49:38 -0700

    testing the commenting on the instructions

--- brett/Lab1/Lab1.srcs/sources_1/bd/BD/hdl/BD_wrapper.v
+++ brett/Lab1/Lab1.srcs/sources_1/bd/BD/hdl/BD_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
-//Date        : Sun May 30 12:15:27 2021
+//Date        : Sun May 30 13:59:24 2021
 //Host        : ece544-VirtualBox running 64-bit Ubuntu 18.04.4 LTS
 //Command     : generate_target BD_wrapper.bd
 //Design      : BD_wrapper
